8b3c90dad2fd9a2b3b0dda126fa9304af91dc48e
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-04-09 19:55:07 +0530

    add method insertAtEnd for SLL

--- src/singlyLinkedList/LinkedList.java
+++ src/singlyLinkedList/LinkedList.java
@@ -5,9 +5,27 @@ public class LinkedList {
 
     public void insertAtBeginning(int data){
         Node node = new Node(data);
-        node.setNext(head);
+        if (!isEmpty()) {
+            node.setNext(head);
+        }
         head = node;
         System.out.println("Data inserted at beginning successfully");
+    }
+
+    public void insertAtEnd(int data){
+        Node node = new Node(data);
+        // if list is empty, create a head with entered node
+        if (isEmpty()){
+            head = node;
+        }
+        else { // traverse till end and then insert
+            Node temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(node);
+        }
+        System.out.println("Data inserted at end successfully");
     }
 
     public boolean isEmpty(){
