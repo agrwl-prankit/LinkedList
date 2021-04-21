df58ac550bf9a7daf1be72d00622e79893bf2e69
agrwl-prankit <agrwnlprankit77@gmail.com>
2021-04-21 21:58:14 +0530

    add insertAtLast for DLL

--- src/doublyLinkedList/DoublyLinkedList.java
+++ src/doublyLinkedList/DoublyLinkedList.java
@@ -14,6 +14,17 @@ public class DoublyLinkedList {
         System.out.println("Data inserted at start successfully");
     }
 
+    public void insertAtEnd(int data){
+        DoublyNode newNode = new DoublyNode(data);
+        DoublyNode temp = head;
+        if (isEmpty()) head = newNode;
+        else {
+            while (temp.getNext() != null) temp = temp.getNext();
+            temp.setNext(newNode);
+            newNode.setPrevious(temp);
+        }
+    }
+
     public boolean isEmpty(){
         return head == null;
     }
